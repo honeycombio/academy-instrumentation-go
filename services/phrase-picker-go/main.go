d315205c71089c5898aaf946c8549b0d4b0f22f8
Jessica Kerr <jessitron@gmail.com>
2024-05-20 13:23:20 -0500

    Update the port number
    this works now

--- services/phrase-picker-go/main.go
+++ services/phrase-picker-go/main.go
@@ -9,43 +9,43 @@ import (
 
 // phrasesList holds the collection of phrases to choose from
 var phrasesList = []string{
-    "Hello, world!",
-    "Today is a wonderful day!",
-    "Programming in Go is fun!",
-    "Keep learning new things.",
-    "Make it work, make it right, make it fast.",
+	"Hello, world!",
+	"Today is a wonderful day!",
+	"Programming in Go is fun!",
+	"Keep learning new things.",
+	"Make it work, make it right, make it fast.",
 }
 
 // Phrase is a struct to map the JSON output
 type Phrase struct {
-    Phrase string `json:"phrase"`
+	Phrase string `json:"phrase"`
 }
 
 func main() {
 
-    // Setup HTTP route and handler
-    http.HandleFunc("/phrase", phraseHandler)
+	// Setup HTTP route and handler
+	http.HandleFunc("/phrase", phraseHandler)
 
-    // Start the HTTP server
-    // Start the HTTP server
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatalf("Failed to start server: %v", err)
-    }
+	// Start the HTTP server
+	// Start the HTTP server
+	err := http.ListenAndServe(":10114", nil)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
 
 func phraseHandler(w http.ResponseWriter, r *http.Request) {
-    // Select a random phrase
-    randomIndex := rand.Intn(len(phrasesList))
-    selectedPhrase := phrasesList[randomIndex]
+	// Select a random phrase
+	randomIndex := rand.Intn(len(phrasesList))
+	selectedPhrase := phrasesList[randomIndex]
 
-    // Create a Phrase struct with the selected phrase
-    response := Phrase{Phrase: selectedPhrase}
+	// Create a Phrase struct with the selected phrase
+	response := Phrase{Phrase: selectedPhrase}
 
-    // Set content type and CORS headers
-    w.Header().Set("Content-Type", "application/json")
-    w.Header().Set("Access-Control-Allow-Origin", "*")
+	// Set content type and CORS headers
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-    // Encode the response as JSON
-    json.NewEncoder(w).Encode(response)
+	// Encode the response as JSON
+	json.NewEncoder(w).Encode(response)
 }
