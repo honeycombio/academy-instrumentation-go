53bab98abaaccde4b40e83c0836c935c4b70efa3
Jessica Kerr <jessitron@gmail.com>
2024-05-20 15:21:57 -0500

    Instrumented

--- services/phrase-picker-go/main.go
+++ services/phrase-picker-go/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"math/rand"
 	"net/http"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/trace"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 // phrasesList holds the collection of phrases to choose from
@@ -22,16 +31,48 @@ type Phrase struct {
 }
 
 func main() {
+	ctx := context.Background()
 
-	// Setup HTTP route and handler
-	http.HandleFunc("/phrase", phraseHandler)
-
-	// Start the HTTP server
-	// Start the HTTP server
-	err := http.ListenAndServe(":10114", nil)
+	// Configure a new OTLP exporter using environment variables for sending data to Honeycomb over gRPC
+	client := otlptracehttp.NewClient()
+	exp, err := otlptrace.New(ctx, client)
 	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+		log.Fatalf("failed to initialize exporter: %e", err)
 	}
+
+	// Create a new tracer provider with a batch span processor and the otlp exporter
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exp),
+	)
+
+	// Handle shutdown to ensure all sub processes are closed correctly and telemetry is exported
+	defer func() {
+		_ = exp.Shutdown(ctx)
+		_ = tp.Shutdown(ctx)
+	}()
+
+	// Register the global Tracer provider
+	otel.SetTracerProvider(tp)
+
+	// Register the W3C trace context and baggage propagators so data is propagated across services/processes
+	otel.SetTextMapPropagator(
+		propagation.NewCompositeTextMapPropagator(
+			propagation.TraceContext{},
+			propagation.Baggage{},
+		),
+	)
+
+	// Implement an HTTP handler func to be instrumented
+	handler := http.HandlerFunc(phraseHandler)
+
+	// Setup handler instrumentation
+	wrappedHandler := otelhttp.NewHandler(handler, "hello")
+	http.Handle("/phrase", wrappedHandler)
+
+	// Start the HTTP server
+	// Start the HTTP server
+	log.Fatal(http.ListenAndServe(":10114", nil))
+
 }
 
 func phraseHandler(w http.ResponseWriter, r *http.Request) {
